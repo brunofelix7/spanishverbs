26a86909d75814beacc1855f6a4f50e4ba2d7105
brunofelix7 <brunofelix.dev@gmail.com>
2022-06-16 18:25:49 -0300

    :package: Criação de fragments para a BottomNavigationView

---
+++ app/src/main/java/me/brunofelix/spanishverbs/ui/account/AccountFragment.kt
@@ -0,0 +1,19 @@
+package me.brunofelix.spanishverbs.ui.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.brunofelix.spanishverbs.R
+
+class AccountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+}
